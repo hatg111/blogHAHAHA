9f88448e239a1f7e3df26b0c189746b7fcf9c8db
tgha <gkxorud111@gmail.com>
2020-02-08 13:58:15 +0900

    myProject 전체를 커밋

---
+++ myProject/src/main/java/com/example/myProject/application/hello/dao/HelloMapper.java
@@ -0,0 +1,12 @@
+package com.example.myProject.application.hello.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@Mapper
+public interface HelloMapper {
+
+
+}
---
+++ myProject/src/main/java/com/example/myProject/configuration/WebConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.myProject.configuration;
+
+//@Configuration
+//@ComponentScan
+//public class WebConfiguration {
+//    public CommonsMultipartResolver multipartResolver(){
+//        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+//        commonsMultipartResolver.setDefaultEncoding("UTF-8");
+//        commonsMultipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024);//5MB
+//        return commonsMultipartResolver;
+//    }
+//}
